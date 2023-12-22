569202c81d009d79c469ec687a396131f840d0b1
AchRef <rahaliashraf732@gmail.com>
2023-12-22 02:19:50 +0100

    msg

---
+++ FrontEnd/FrontEnd/Models/Compte.cs
@@ -0,0 +1,16 @@
+﻿namespace FrontEnd.Models
+{
+	public class Compte
+	{
+		public string Code { get; set; }
+		public float Solde { get; set; }
+		public DateTime DateCreation { get; set; }
+		public Compte(string code, float solde, DateTime dateCreation)
+		{
+			Code = code;
+			Solde = solde;
+			DateCreation = dateCreation;
+		}
+	}
+
+}
